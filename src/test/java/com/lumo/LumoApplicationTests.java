d97dcfd296909b0a8bdfb32c02d9399f174ce258
Diego Maidana <die.maidana1@gmail.com>
2025-07-28 14:17:46 -0300

    Initial commit

---
+++ src/test/java/com/lumo/LumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
